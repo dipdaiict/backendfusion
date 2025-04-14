481836cb33f79a387e0ead1b15ef4e72dbef8ffd
Dip Patel <dippatel256@gmail.com>
2025-04-15 00:49:55 +0530

    Added /health endpoint for service status check.

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
-from app.api import routes_user, routes_auth
-from app.middleware import setup_middlewares
-from app.logging_conf import configure_logging
+from app.api import routes_system
+# from app.middleware import setup_middlewares
+# from app.logging_conf import configure_logging
 
 app = FastAPI()
 
-configure_logging(app)
-setup_middlewares(app)
+# configure_logging(app)
+# setup_middlewares(app)
 
-app.include_router(routes_user.router, prefix='/users')
-app.include_router(routes_auth.router, prefix='/auth')
+# app.include_router(routes_user.router, prefix='/users')
+# app.include_router(routes_auth.router, prefix='/auth')
+app.include_router(routes_system.router, prefix='/system')
